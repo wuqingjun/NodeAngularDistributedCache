0fcbe78259a48aaf3f2fe364a123c3abc20344dd
wadewu <wuqingjun@gmail.com>
2015-06-09 11:01:04 -0700

    Cache class

---
+++ Cache/cache.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+    Cache : {
+    Version: '1.0',
+    objects: [],
+    push: function(k, v) {
+    return this.objects.push({ k: v });
+    }
+},
+
+    testonly: function () {
+    
+    }
+}
+
