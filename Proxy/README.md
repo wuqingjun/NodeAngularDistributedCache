9c62015b1e8a885feacdf2bc1a1f594b78aba3cc
bencordova <ben.cordova@gmail.com>
2015-06-28 15:56:20 -0600

    Add baseline infrastructure to support documented API in the proxy server.

--- Proxy/README.md
+++ Proxy/README.md
@@ -1,3 +1,15 @@
-﻿# Proxy
+﻿# Load Balancer Proxy
 
+The Load Balancer or Proxy will be a RESTful HTTP(s) server which will field implement the standard RESTful functions. 
+This server will forward these requests to the appropriate Cache Server based on some balancing scheme we will develop. The Load Balancer Server will be implemented in NodeJS. All responses to /data and /server will return in a JSON format.  
 
+This API table is modeled after the framework detailed here: https://en.wikipedia.org/wiki/Representational_state_transfer#Example. 
+
+| URI | GET | PUT | POST | DELETE |
+|-----|-----|-----|------|--------|
+| /, /index | Serve the Client Application | Not Supported | Not Supported | Not Supported |
+| /app/<resource> | Support space for Client Application | Not Supported | Not Supported | Not Supported |
+| /data | List details about the Load Balancer Server's data service and all URIs available. | Not Supported | Not Supported | Not Supported |
+| /data/<key> | Retrieve <value> for <key> from the system. | Store or replace <key> and its <value> in the system. | Not Supported | Delete <key> and its <value> from the system. |
+| /server | Lists all available Cache server and their status | Not Supported | Register a new Cache Server. | Delete all Cache Servers. |
+| /server/<id> | List details about Cache Server <id>. | Replace an existing Cache Server <id>. | Not Supported | Delete Cache Server <id>. |
--- Proxy/proxy.js
+++ Proxy/proxy.js
@@ -1,44 +1,226 @@
-﻿var http = require("http");
+﻿//var http = require("http");
 var Cache = require('../Common/cache.js');
 var net = require("net");
+var restify = require('restify');
+var crypto = require('crypto');
 
 var globalCache = new Cache();
 
-var httpserver = http.createServer(function (request, response) {
-    get('k1', request, response);
+PORT = 8080;
+
+
+//  ------------  //
+//  Server Setup  //
+//  ------------  //
+var server = restify.createServer({
+    name: 'Load Balancing Proxy',
 });
 
-httpserver.listen(8080);
+// uncomment for nifty curl support
+// server.pre(restify.pre.userAgentConnection());
 
-setInterval(function() {
-    if (!globalCache.Executing) {
+server.use(restify.bodyParser());
+
+
+//  ---------------------  //
+//  Cache Server Handling  //
+//  ---------------------  //
+var cacheServers = [// TO DO: start out with empty list and update everything to react appropriately
+                    {
+                        id: 0,
+                        connectionInfo: { port: 8124 }
+                    }];
+
+var nextServerId = cacheServers.length;
+function nextId() {
+    return nextServerId++;
+}
+
+function selectServer(req) {
+
+    // This function selects a 'random' cache server based on an md5 hash of the incoming request.
+
+    if (cacheServers.length == 0) {
+        return undefined;
     }
-}, 3000);
 
-function get(k, request, response) {
-    var client = net.connect({ port: 8124 },
+    var unhashedString = "" + req;
+    var hashed = crypto.createHash('md5').update(unhashedString).digest('hex').substring(0,4);
+    var integer = parseInt(hashed, 16);
+    var idx = integer % cacheServers.length;
+    return cacheServers[idx].connectionInfo;
+}
+
+
+
+//  ------------------  //
+//  Client Application  //
+//  ------------------  //
+function sendApp(req, res, next) {
+    res.send("TO DO: send down the AngularJS Client Application");
+    return next();
+}
+
+server.get('/', sendApp);
+server.get('/index', sendApp);
+
+// Placeholder for application support URI
+//server.get('/app/:resource', function (req, res, next) { });
+
+
+
+//  -----  //
+//  /data  //
+//  -----  //
+server.get('/data/:key', function (req, res, next) { // retrieve :key value
+
+    var serverOptions = selectServer(req); // select a random server
+
+    if (serverOptions === undefined) {
+        console.log("No cache servers available");
+        res.send("");
+        return next();
+    }
+
+    var client = net.connect(serverOptions,
     function () {
         console.log('connected to server ascii!');
-        client.write(k, 'ascii');
+        console.log('requesting ' + req.params.key);
+
+        // TO DO: Update client server code to be RESTful
+        client.write(req.params.key, 'ascii');
     });
+
     client.on('data', function (data) {
-        var v = data.toString();
-        console.log(v);
+        var val = data.toString();
+        console.log('retrieved ' + req.params.key + ':' + val);
+        res.send(val);
         client.end();
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<!DOCTYPE 'html'>");
-        response.write("<html>");
-        response.write("<head>");
-        response.write("<title>Hello World Page</title>");
-        response.write("</head>");
-        response.write("<body>");
-        response.write(v);
-        response.write("</body>");
-        response.write("</html>");
-        response.end();
     });
+
     client.on('end', function () {
         console.log('disconnected from server');
     });
-}
 
+    return next();
+});
+
+
+server.put('/data/:key', function (req, res, next) { // store or replace :key
+
+    var serverOptions = selectServer(req); // select a random server
+
+    if (serverOptions === undefined) {
+        console.log("No cache servers available");
+        res.send("");
+        return next();
+    }
+
+    console.log("TO DO: save/overwrite value '" + req.params.value + "' for key '" + req.params.key + "'");
+    res.send("");
+    return next();
+});
+
+server.del('/data/:key', function (req, res, next) { // delete :key
+
+    var serverOptions = selectServer(req); // select a random server
+
+    if (serverOptions === undefined) {
+        console.log("No cache servers available");
+        res.send("");
+        return next();
+    }
+
+    console.log("TO DO: delete key '" + req.params.key + "'");
+    res.send(true);
+    return next();
+});
+
+
+
+//  --------  //
+//  /servers  //
+//  --------  //
+server.get('/servers', function (req, res, next) { // list all servers
+    // TO DO: make this prettier
+    res.send(cacheServers);
+    return next();
+});
+
+server.post('/servers', function (req, res, next) { // add a new server
+    // TO DO: test this, validate incoming parameter data
+    var newServer = { id: nextId(), connectionInfo: req.params };
+    cacheServers.push(newServer);
+    req.send(true);
+    return next();
+});
+
+server.del('/servers', function (req, res, next) { // delete all servers
+    // TO DO: test this (cuz it's kinda scary)
+    cacheServers = [];
+    req.send(true);
+    return next();
+});
+
+server.get('/servers/:id', function (req, res, next) { // get info for server :id
+    // TO DO: make this prettier
+    for (var idx = 0; idx < cacheServers.length; idx++) {
+        if (cacheServers[idx].id == req.params.id) {
+            res.send(cacheServers[idx]);
+            return next()
+        }
+    }
+    res.send("Server not found");
+    return next();
+});
+
+
+server.put('/servers/:id', function (req, res, next) { // replace server :id
+    // TO DO: test this
+
+    for (var idx = 0; idx < cacheServers.length; idx++) {
+        if (cacheServers[idx].id == req.params.id) {
+            cacheServers[idx] = {id: req.params.id, connectionInfo: req.params.connectionInfo};
+            res.send(true);
+            return next()
+        }
+    }
+    console.log("Server not found");
+    res.send(false);
+    return next();
+});
+
+server.del('/servers/:id', function (req, res, next) { // delete server :id
+    // TO DO: test this
+
+    // loop through all servers until we find the id to delete
+    //  if it's the last one, pop it off the list
+    //  else, put the last one in its place (order doesn't matter)
+    for (var idx = 0; idx < cacheServers.length; idx++) {
+        if (cacheServers[idx].id == req.params.id) {
+            var last = cacheServers.pop();
+            if (idx != cacheServers.length - 1) {
+                cacheServers[idx] = last;
+            }
+            res.send(true);
+            return next()
+        }
+    }
+    console.log("Server " + req.params.id + "not found");
+    res.send(false);
+    return next();
+});
+
+
+//  --------------  //
+//  Run the server  //
+//  --------------  //
+server.listen(8080, function () {
+    console.log("Load Balancing Proxy Server RESfully Listening on: http://localhost:%s", PORT);
+});
+
+
+setInterval(function() { // what does this do?
+    if (!globalCache.Executing) {
+    }
+}, 3000);
