38291790a31e5ecfd9167cd6e35166d1b148431e
wadewu <wuqingjun@gmail.com>
2015-07-03 08:47:54 -0700

    code cannot put in index.js

--- Proxy/public/index.js
+++ Proxy/public/index.js
@@ -1,8 +1,1 @@
-﻿//'use strict';
-
-var app = angular.module("myApp", []);
-app.controller("myCtrl", function ($scope) {
-    $scope.records = [{ key: "k1", value: 'v1' }, { key: "k2", value: 'v2' }];
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-});
+﻿
